7d785a1a985ea0721be4b954845991948f80d4e1
Pranav Shirke <pranavshirke@Pranavs-MacBook-Pro.local>
2020-06-17 09:45:27 -0400

    merged calculator and deprication into calculator

--- src/app/pages/depreciation-calculator/components/calculator/calculator.component.ts
+++ src/app/pages/depreciation-calculator/components/calculator/calculator.component.ts
@@ -3,6 +3,8 @@ import { AgGridAngular } from 'ag-grid-angular';
 import { HttpClient } from '@angular/common/http';
 import { ListedPropertyTypeService } from '../../services/listed-property-type.service';
 import { ListedPropertyType } from '../../models/listed-property-type';
+import { AssetService } from '../../services/asset.service';
+import { Asset } from '../../models/asset';
 
 @Component({
   selector: 'kod-calculator',
@@ -11,49 +13,84 @@ import { ListedPropertyType } from '../../models/listed-property-type';
 })
 export class CalculatorComponent implements OnInit {
   @ViewChild(AgGridAngular, { static: true }) agGrid: AgGridAngular;
-  columnDefs = [
+  /*columnDefs = [
       {headerName: 'Id', field: 'id', sortable: true, filter: true ,filterParams: {
           buttons: ['reset', 'apply'],
-        }/*,checkboxSelection: true*/ , editable: true ,resizable: true ,suppressSizeToFit: false},
+        },checkboxSelection: true , editable: true ,resizable: true ,suppressSizeToFit: false},
       {headerName: 'Description', field: 'description', sortable: true, filter: true , filterParams: {
           buttons: ['reset', 'apply']}, editable: true, resizable: true ,suppressSizeToFit: false  },
       {headerName: 'Asset', field: 'asset', sortable: true, filter: true ,filterParams: {
           buttons: ['reset', 'apply']}, editable: true, resizable: true ,suppressSizeToFit: false}
+  ];*/
+  columnDefs = [
+    {headerName: 'ID', field: 'id', sortable: true, filter: true,filterParams: {
+      buttons: ['reset', 'apply'],
+    }/*,checkboxSelection: true*/ , editable: true },
+    {headerName: 'Asset Number', field: 'assetNumber', sortable: true, filter: true,filterParams: {
+      buttons: ['reset', 'apply'],
+    }, editable: true, resizable:true },
+    {headerName: 'Asset Category', field: 'assetCategoryId', sortable: true, filter: true ,filterParams: {
+      buttons: ['reset', 'apply']}, editable: true, resizable:true },
+    {headerName: 'Description', field: 'description', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Date Placed in Service', field: 'dateInService', sortable: true, filter: true , filterParams: {
+      buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Cost', field: 'cost', sortable: true, filter: true , filterParams: {
+       buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Business Percentage', field: 'businessPercentage', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Business Percentage', field: 'businessPercentage', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Listed Property Type', field: 'listedPropertyTypeId', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true  , resizable:true },
+    {headerName: 'Method', field: 'methodId', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Life', field: 'life', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Prior Regular Depreciation', field: 'priorRegDepreciation', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Prior Bonus Depreciation', field: 'priorBonusDepriciation', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Prior Sec 179 Expense', field: 'priorExpSec179', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Property Type (Code Section)', field: 'propertyTypeCodeId', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true, resizable:true  },
+    {headerName: 'If Amortization Code Section', field: 'amortizationCodeId', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Asset Convention', field: 'assetConvention', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Current Depreciation', field: 'currentDepriciation', sortable: true, filter: true , filterParams: {
+      buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Section 179 Expense for Current Year', field: 'currentYearExpSec179', sortable: true, filter: true , filterParams: {
+      buttons: ['reset', 'apply']}, editable: true , resizable:true  },
+    {headerName: 'Bonus Deprication', field: 'bonusDepriciation', sortable: true, filter: true , filterParams: {
+        buttons: ['reset', 'apply']}, editable: true , resizable:true  }
+              
+           
+      
   ];
-  
 
-  private propTypes: ListedPropertyType[];
+  //private propTypes: ListedPropertyType[];
+  private propTypes: Asset[];
 
-  /*columnDefs = [
-    { headerName: 'Make', field: 'make', editable: true, rowGroup: true },
-    { headerName: 'Life', field: 'model', editable: true }
-  ];
-
-  autoGroupColumnDef = {
-    headerName: 'Model',
-    field: 'model',
-    cellRenderer: 'agGroupCellRenderer',
-    cellRendererParams: {
-      checkbox: true
-    }
-  };
- */
   rowData: any;
 
   constructor(
     private http: HttpClient,
-    private _listedPropTypeService: ListedPropertyTypeService) {
+    private _listedPropTypeService: ListedPropertyTypeService,
+    private _assetService: AssetService) {
   }
 
+
   ngOnInit() {
-    this._listedPropTypeService.getAll().subscribe(proptypes => {
+    //this._listedPropTypeService.getAll().subscribe(proptypes => {
+      this._assetService.getAll().subscribe((proptypes) => {
       this.propTypes = proptypes;
       console.log("prop types", this.propTypes);
       this.rowData = this.propTypes;
       this.agGrid.api.sizeColumnsToFit();
     });
 
-    //this.rowData = this.http.get('https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/smallRowData.json');
   }
 
   getSelectedRows() {
