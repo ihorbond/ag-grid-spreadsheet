621dc5409858b1813f740cbe645b07f39e511de7
Pranav Shirke <pranavshirke@Pranavs-MacBook-Pro.local>
2020-06-20 18:17:44 -0400

    added date picker

--- src/app/pages/depreciation-calculator/services/grid-columns.service.ts
+++ src/app/pages/depreciation-calculator/services/grid-columns.service.ts
@@ -149,7 +149,7 @@ export class GridColumnsService {
                     cellRenderer: 'dateTimeRenderer',
                     //filterParams: {
                     //    buttons: ['reset', 'apply']
-                    //}
+                    //  }
                 },
                 {
                     headerName: 'Cost', 
